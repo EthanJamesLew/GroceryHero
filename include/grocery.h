6c1f60727f465d97c5f7ae9239c89079d8b6425f
Ethan Lew <ethan.lew@pcc.edu>
2016-09-30 18:50:36 -0700

    _items array

--- include/grocery.h
+++ include/grocery.h
@@ -1,6 +1,8 @@
 #ifndef GROCERY_H
 #define GROCERY_H
 #include "grocery_item.h"
+
+#define ITEM_LEN 256
 /*
 Grocery models a shopping list. It handles an array of grocery items.
 */
@@ -11,15 +13,15 @@ public:
 	~Grocery();
 
 	void printItems();
-	void addItem(char* name, double price, int quantity);
+	void addItem(char* name, double price, double quantity);
 	void deleteItem(char* name);
-	void editItem(char* name, double price, int quantity);
+	void editItem(char* name, double price, double quantity);
 	double getTotal();
 
 	bool isItem(char* name);
 
 private:
-	Grocery_Item* _items;
+	Grocery_Item _items[ITEM_LEN];
 	int _size;
 };
 #endif
